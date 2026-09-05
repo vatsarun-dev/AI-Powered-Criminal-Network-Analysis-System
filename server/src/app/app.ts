f1a3072a9a9d45d49906349da5cb98ae5f07abae
Arun Vats <vatsarun58@gmail.com>
2026-09-05 15:27:40 +0530

    Update import paths to TypeScript files

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,8 +1,8 @@
 import express, { type Express } from "express";
-import authRoutes from "../modules/auth/auth.routes.js";
-import errorHandler from "../middlewares/error.middleware.js";
-import securityMiddleware from "../middlewares/security.middleware.js";
-import fileRoutes from "../modules/File/file.routes.js";
+import authRoutes from "../modules/auth/auth.routes.ts";
+import errorHandler from "../middlewares/error.middleware.ts";
+import securityMiddleware from "../middlewares/security.middleware.ts";
+import fileRoutes from "../modules/File/file.routes.ts";
 
 export default function createApp(): Express {
   const app = express();

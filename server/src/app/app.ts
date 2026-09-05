a468aa5b4003a4afb5c47827f748bba10be41ff8
Arun Vats <vatsarun58@gmail.com>
2026-09-05 15:26:15 +0530

    Remove cookie parser middleware
    Removed cookie parser middleware from app setup.

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -2,13 +2,11 @@ import express, { type Express } from "express";
 import authRoutes from "../modules/auth/auth.routes.js";
 import errorHandler from "../middlewares/error.middleware.js";
 import securityMiddleware from "../middlewares/security.middleware.js";
-import cookieParser from "cookie-parser";
 import fileRoutes from "../modules/File/file.routes.js";
 
 export default function createApp(): Express {
   const app = express();
 
-  app.use(cookieParser());
 
   securityMiddleware(app);
 
